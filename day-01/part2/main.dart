28f4660ba39903c5ee1388996599ebf47d41ddd4
mjr2595 <michaeljohnraymond@gmail.com>
2023-12-02 01:01:09 -0600

    day 1, but only part 1

---
+++ day-01/part2/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+void main() async {
+  final file = File('test_input.txt');
+  Stream<String> lines = file
+      .openRead()
+      .transform(utf8.decoder) // Decode bytes to UTF-8.
+      .transform(LineSplitter()); // Convert stream to individual lines.
+  try {
+    num total = 0;
+    await for (var line in lines) {
+      num lineAmount = getLineAmount(line) ?? 0;
+      total += lineAmount;
+      print('$line: ${lineAmount}, total: ${total}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
+// Strip away the letters from the string, then add up the last two numbers.
+getLineAmount(String line) {
+  if (line.isEmpty) return null;
+
+  String stringifiedNums = '';
+  line.split('').forEach((ch) {
+    if (int.tryParse(ch) != null) {
+      stringifiedNums += ch;
+    }
+  });
+  if (stringifiedNums.length > 0) {
+    stringifiedNums =
+        stringifiedNums[0] + stringifiedNums[stringifiedNums.length - 1];
+    return int.parse(stringifiedNums);
+  }
+}
